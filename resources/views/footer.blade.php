b625b392d80d4536ab65160c7428fccf680c17c3
isauraqinthara <isauraqinthara28@gmail.com>
2023-11-30 14:58:34 +0700

    Pertemuan 30 Nop 2023

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<br/>
+    <br/>
+    <hr/>
+    <footer>
+        <p>&copy; Isaura Qinthara Heriswan. 2023</p>
+    </footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+@extends('master2')
+@section('judulhalaman', 'Data Pegawai')
+
+@section('konten')
+    <h2>www.malasngoding.com</h2>
+	<h3>Data Pegawai</h3>
+
+	<a href="/pegawai/tambah" class="btn-primary"> + Tambah Pegawai Baru</a>
+
+	<br/>
+    <p>Cari Data Pegawai :</p>
+	<form action="/pegawai/cari" method="GET">
+		<input type="text" class="form-control" name="cari" placeholder="Cari Pegawai .."
+        value="{{ old("cari", isset($cari) ? $cari : '') }}"> <!-- menampilan secara otomatis apa yang dicari -->
+		<input type="submit" value="CARI" class="btn btn-info">
+	</form>
+	<br/>
+
+	<table class="table table-striped table-hover">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+				|
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+
+    <!-- metode untuk membuat
+    {{ $pegawai->links() }}
+
+@endsection
